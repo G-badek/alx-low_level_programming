cb90c391f71f532a278684c0bfcf4ca07813a8c4
Akinjibade Fortunate <akinjibadefortunate@gmail.com>
2021-06-17 10:56:55 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
  *
- *Return - Always 0 (Success)
+ * Return - Always 0 (Success)
  */
 int main(void)
 {
@@ -17,6 +17,6 @@ printf("Size of a char: %lu byte(s)\n", sizeof(charType));
 printf("Size of an int: %lu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %lu byte(s)\n", sizeof(longIntType));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(longLongIntType));
-printf("Size of a float: %lu byte(s)\n", sizeof(longLongIntType));
+printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
 return (0);
 }
