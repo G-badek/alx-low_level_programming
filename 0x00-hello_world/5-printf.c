7a749295d0c48b7d24f6cf6b9e42760231dd3e3d
Akinjibade Fortunate <akinjibadefortunate@gmail.com>
2021-06-17 09:39:07 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *Main: Entry point
+ *
+ *
+ *Return: Always return 0(success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
