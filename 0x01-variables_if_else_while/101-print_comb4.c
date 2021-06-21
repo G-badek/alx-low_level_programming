9b9fe89cb1de0a66552400f62bca0aa0864c7dc0
Akinjibade Fortunate <akinjibadefortunate@gmail.com>
2021-06-21 12:28:56 -0700

    Write a program that prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,21 +5,21 @@
  * No Description:
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 int x;
 int y;
 int z;
 for (x = 0; x < 9; x++)
 {
-for (y = x + 1; y < 9; y++ )
+for (y = x + 1; y < 9; y++)
 {
 for (z = y + 1; z < 9; z++)
 {
 putchar(x + '0');
 putchar(y + '0');
 putchar(z + '0');
-if (x > 7)
+if (x < 7)
 {
 putchar(',');
 putchar(' ');
