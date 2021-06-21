61bc00158f15adf295be95e47dd798f5ca5d5cef
Akinjibade Fortunate <akinjibadefortunate@gmail.com>
2021-06-21 13:46:04 -0700

    Write a program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -26,7 +26,7 @@ putchar(x + '0');
 putchar(' ');
 putchar(y + '0');
 putchar(z + '0');
-if ((w != '9') || (x != '8') || (y != '9') || (z != '9'))
+if ((w + x + y + z) < 36)
 {
 putchar(',');
 putchar(' ');
