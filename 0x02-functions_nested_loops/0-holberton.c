994451d9231714a6664045e0ab38d285736a205d
Akinjibade Fortunate <akinjibadefortunate@gmail.com>
2021-06-22 11:14:18 -0700

    Write a program that prints Holberton, followed by a new line

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,5 +8,12 @@
  */
 int main(void)
 {
+int i;
+char p[] = "Holberton";
+for (i = 0; i < 9; i++)
+{
+_putchar(p[i]);
+}
+_putchar('\n');
 return (0);
 }
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,2 +1,6 @@
-char name[] = Holberton;
-printf("%s\n", name);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar.c(char c);
+
+#endif
