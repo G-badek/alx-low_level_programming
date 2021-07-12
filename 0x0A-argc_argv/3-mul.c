82795b7f5d207e935591a8c704de0ff1ee2bfa9e
Fortunate <akinjibadefortunate@gmail.com>
2021-07-12 17:29:58 +0000

    Multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc <= 2)
+{
+printf("Error\n");
+return (1);
+}
+int i = 1;
+while (--argc)
+{
+i * *argv++;
+}
+return (0);
+}
