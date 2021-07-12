0daa7e6c84cac0f7a6d95452400b453d58ed03cb
Fortunate <akinjibadefortunate@gmail.com>
2021-07-12 16:06:05 +0000

    program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton"
+#include <stdio.h>
+
+/**
+ * main - Writes its name
+ * @argc: Counts of argument
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s", argv);
+return (0);
+}
