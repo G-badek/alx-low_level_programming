26cf4068a57df2a9faeb22db1751d3c8deb44035
Fortunate <akinjibadefortunate@gmail.com>
2021-07-14 20:27:54 +0000

    Write a function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -27,9 +27,8 @@ size++;
 size++;
 }
 size++;
-/* allocate memory for total number of chars and
- * new line for each word
- */
+/* allocate memory for total number of chars and */
+/* new line for each word */
 strDup = malloc(sizeof(char) * size);
 if (strDup == NULL)
 {
