9f5b207712b68b7f3069f6830f7f851043b0169e
Fortunate <akinjibadefortunate@gmail.com>
2021-07-14 18:52:49 +0000

    Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton"
+#include <stdio.h>
+
+/**
+ * _strdup - Returns a pointer to a new string
+ * @str: Strings of characters
+ * Return: NULL if str = NULL
+ */
+char *_strdup(char *str)
+{
+	char *strDup;
+	int i, j;
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+	i++;
+	}
+	i++;
+	strDup = malloc(sizeof(char) * i);
+	if (strDup == NULL)
+	{
+	return (NULL);
+	}
+	j = 0;
+	while (str[j] != '\0')
+	{
+	strDup[j] = str[j];
+	j++;
+	}
+	return (strDup);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,5 +2,6 @@
 #define HOLBERTON_H
 
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif
