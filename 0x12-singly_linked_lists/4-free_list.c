b09ae1d5a200bf436c63af89b1f33d8792eedea3
Fortunate <akinjibadefortunate@gmail.com>
2021-07-30 08:46:15 +0000

    frees a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: linked lists
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+if (head)
+{
+free_list(head->next);
+free(head->str);
+free(head);
+}
+}
