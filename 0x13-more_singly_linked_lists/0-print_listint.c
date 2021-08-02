815c4d018b2d255533ab15bad3be10f723975eaa
Fortunate <akinjibadefortunate@gmail.com>
2021-08-02 12:31:16 +0000

    prints all the elements of a listint_t list.

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: head of the list
+ * Return: size_t type
+ */
+size_t print_listint(const listint_t *h)
+{
+count = 0;
+while (h)
+{
+count++;
+printf("%d\n", h->n);
+h = h->next;
+}
+return (count);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,9 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t print_listint(const listint_t *h);
+
+#endif
