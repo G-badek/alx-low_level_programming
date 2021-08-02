512997e5d7917ea2bb36144c30297851323152bb
Fortunate <akinjibadefortunate@gmail.com>
2021-08-02 16:49:41 +0000

    reverses a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,12 @@
+#include "list.h"
+
+/**
+ * reverse_listint - delete function
+ * @head: describe argument
+ * Return: what does it return?
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+head++;
+return (0);
+}
