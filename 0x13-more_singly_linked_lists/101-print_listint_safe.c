4ffbccf5d496507b7265a5a089d84b984c2844c7
Fortunate <akinjibadefortunate@gmail.com>
2021-08-02 17:00:06 +0000

    Prints a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * size_t print_listint_safe - prints a listint_t linked list
+ * @head: head of list
+ * Return: the number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+head++;
+return (0);
+}
