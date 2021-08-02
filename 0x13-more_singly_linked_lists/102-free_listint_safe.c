9eaa6a13e357f97814b5f4339f36ee3b523ab3e1
Fortunate <akinjibadefortunate@gmail.com>
2021-08-02 17:22:33 +0000

    Frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t list
+ * @h: head of the list
+ * Returns: return listint
+ */
+size_t free_listint_safe(listint_t **h)
+{
+h++;
+return (0);
+}
