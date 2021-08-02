0feed27180e2694a153401b19feb4c3d812863d3
Fortunate <akinjibadefortunate@gmail.com>
2021-08-02 17:29:25 +0000

    Finds the loop in a linked list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * find_listint_loop - Finds the loop in a linked list
+ * @head: head of list
+ * Return: return loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+head++;
+return (0);
+}
