6b7e5d7643d6585bb2bb503a6db6d8bb953d4885
Fortunate <akinjibadefortunate@gmail.com>
2021-08-05 02:47:58 +0000

    Prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_bin - prints the binary
+ * @n: number in decimal
+ * Return: void
+ */
+void rev_bin(unsigned long int n)
+{
+if (!n)
+{
+return;
+}
+rev_bin(n >> 1);
+_putchar((n & 1) + '0');
+}
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number in decimal
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+if (!n)
+{
+_putchar('0');
+}
+else
+{
+rev_bin(n);
+}
+}
