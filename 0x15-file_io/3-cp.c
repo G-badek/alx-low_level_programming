6611c8ecb6db19d147209f664ee2f64b42c4d724
Fortunate <akinjibadefortunate@gmail.com>
2021-08-10 00:26:53 +0000

    program that copies the content of a file to another file.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,59 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * main - copy program
+ * @argc: count parameters
+ * @argv: array to parameters
+ * Return: 0 success or another value fail
+ */
+int main(int argc, char *argv[])
+{
+int file_one, file_two, r, w, c;
+char buffer[1024];
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usafe: cp file_from file_to\n"),
+	exit(97);
+}
+file_one = open(argv[1], O_RDONLY);
+if (file_one == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+		argv[1]), exit(98);
+}
+file_two = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (file_two == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+}
+for (r = 1024; r == 1024;)
+{
+r = read(file_one, buffer, 1024);
+if (r == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+}
+w = write(file_two, buffer, r);
+if (w == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+}
+}
+c = close(file_one);
+if (c == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_one), exit(100);
+}
+c = close(file_two);
+if (c == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_two), exit(100);
+}
+return (0);
+}
