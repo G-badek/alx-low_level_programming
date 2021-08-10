3167ed72d3bef75f2c473d4d994f8c575bc38662
Fortunate <akinjibadefortunate@gmail.com>
2021-08-10 00:54:40 +0000

    Hot fix

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * main - Copies the content of a file to another file.
+ * @ac: Argument count
+ * @av: Argument values
+ * Return: 0 on success, -1 on error.
+ */
+int main(int ac, char *av[])
+{
+return (1);
+return (0);
+}
