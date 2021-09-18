18cb5189c37eacec34c6d59fd4d0ef199f244da3
Fortunate <akinjibadefortunate@gmail.com>
2021-09-18 03:45:14 +0000

    insert Node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,47 +1,57 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position.
- * @h: double linked list
- * @idx: index double linked list
- * @n: number member
- * Return: the address of the new node, or NULL if it failed
- */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+ * insert_dnodeint_at_index - inserts a new node at given index in the list
+ * @head: pointer to head of the list
+ * @idx: index to add at, starting from 0
+ * @n: value of new node
+ * Return: new node or null
+ **/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 {
-dlistint_t *new = NULL, *tmp = NULL;
-unsigned int i = 1;
-	ew = malloc(sizeof(dlistint_t));
-if (new == NULL || h == NULL)
+unsigned int count;
+dlistint_t *tmp, *new, *tmp_prev;
+if (head == NULL && idx > 0)
 {
 return (NULL);
 }
-new->n = n;
-tmp = *h;
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+{
+return (NULL);
+}
+new->n = n, new->prev = new->next = NULL;
 if (idx == 0)
 {
-return (add_dnodeint(h, n));
-}
-while (tmp->next)
+if (*head)
 {
-if (i == idx)
-{
-new->prev = tmp;
-new->next = tmp->next;
-tmp->next = new;
-new->next->prev = new;
+new->next = *head;
+(*head)->prev = new, *head = new;
 }
-tmp = tmp->next;
-i++;
-}
-if (i == idx)
+else
 {
-return (add_dnodeint_end(h, n));
-}
-if (i < idx)
-{
-free(new);
-return (NULL);
+*head = new;
 }
 return (new);
+}
+count = 1, tmp = (*head)->next;
+while (tmp)
+{
+if (idx == count)
+{
+tmp->prev->next = new, new->prev = tmp->prev;
+new->next = tmp, tmp->prev = new;
+return (new);
+}
+count++;
+tmp_prev = tmp;
+tmp = tmp->next;
+}
+if (tmp == NULL && count == idx)
+{
+tmp_prev->next = new, new->prev = tmp_prev;
+return (new);
+}
+free(new);
+return (NULL);
 }
