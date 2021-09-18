62048ea58542e70e0d2bbbbf159a12936e99b621
Fortunate <akinjibadefortunate@gmail.com>
2021-09-18 02:42:22 +0000

    Add node

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,26 +8,28 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *tmp;
-	new = malloc(sizeof(dlistint_t));
-	if (!new){
-		return (NULL);
-	}
-	new->n = n;
-	new->next = NULL;
-	tmp = *head;
-	if (!(tmp))
-	{
-		new->prev = NULL;
-		*head = new;
-	}
-	else
-	{
-		while (tmp->next){
-			tmp = tmp->next;
-		}
-		tmp->next = new;
-		new->prev = tmp;
-	}
-	return (new);
+dlistint_t *new, *tmp;
+new = malloc(sizeof(dlistint_t));
+if (!new)
+{
+return (NULL);
+}
+new->n = n;
+new->next = NULL;
+tmp = *head;
+if (!(tmp))
+{
+new->prev = NULL;
+*head = new;
+}
+else
+{
+while (tmp->next)
+{
+tmp = tmp->next;
+}
+tmp->next = new;
+new->prev = tmp;
+}
+return (new);
 }
