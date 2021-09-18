8a754e5a36f7a0e1571329651254ad388a5ca3cb
Fortunate <akinjibadefortunate@gmail.com>
2021-09-18 02:46:46 +0000

    Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free a dlistint_t list.
+ * @head: double linked list
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *current;
+if (!head)
+{
+return;
+}
+while (head)
+{
+current = head;
+head = head->next;
+free(current);
+}
+free(head);
+}
