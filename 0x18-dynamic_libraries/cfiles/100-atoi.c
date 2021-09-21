78d92b977ac5cbdfdc919a8d687dbc8d15dd795e
Fortunate <akinjibadefortunate@gmail.com>
2021-09-21 01:33:45 +0000

    Dynamic files

---
+++ 0x18-dynamic_libraries/cfiles/100-atoi.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#define UNUSED(x) (void)(x)
+
+/**
+ *  * _atoi - Entry point
+ *   * @s: input
+ *    * Return: Always 0 (Success)
+ *     */
+int _atoi(char *v)
+{
+	UNUSED(v);
+	return (0);
+}
